c3e9f078a2d7a8b9a268b5346018cfaa21e9ec14
Ananth Chandrasekharan <ananthchandrasekharan@gmail.com>
2025-01-30 23:25:43 +0530

    Modifications

--- 01-Vulkan/01-Windows/02-Bluescreen/02-InstanceExtensions/Build.bat
+++ 01-Vulkan/01-Windows/02-Bluescreen/02-InstanceExtensions/Build.bat
@@ -2,12 +2,9 @@ cls
 
 del Vk.exe Log.txt
 
-cl.exe /c /EHsc /I:C:\VulkanSDK\Vulkan\Include Vk.c
+cl /I"%VULKAN_SDK%\Include" /c Vk.c /Fo"Vk.obj"
 
-rc.exe Vk.rc
-
-link.exe Vk.obj Vk.res /LIBPATH:C:\VulkanSDK\Vulkan\Lib Kernel32.lib User32.lib GDI32.lib /SUBSYSTEM:WINDOWS /MACHINE:x64
-
-del Vk.res Vk.obj
+link Vk.obj /LIBPATH:"%VULKAN_SDK%\Lib" vulkan-1.lib user32.lib gdi32.lib kernel32.lib /OUT:Vk.exe
 
 Vk.exe
+
