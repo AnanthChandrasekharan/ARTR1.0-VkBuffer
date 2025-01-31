2dff0d9310f0494a6824686274d0dbb030291181
Ananth Chandrasekharan <ananthchandrasekharan@gmail.com>
2025-01-31 16:22:54 +0530

    Corrections to bat file as per Gokhale Sirs template

--- 01-Vulkan/01-Windows/02-Bluescreen/02-InstanceExtensions/Build.bat
+++ 01-Vulkan/01-Windows/02-Bluescreen/02-InstanceExtensions/Build.bat
@@ -4,9 +4,11 @@ del Vk.exe Log.txt
 
 cl /I"C:\VulkanSDK\Vulkan\Include" /c Vk.c /Fo"Vk.obj"
 
-link Vk.obj /LIBPATH:"C:\VulkanSDK\Vulkan\Lib" vulkan-1.lib user32.lib gdi32.lib kernel32.lib /OUT:Vk.exe 
+rc.exe Vk.rc
 
-del Vk.obj Vk.pdb
+link Vk.obj Vk.res /LIBPATH:"C:\VulkanSDK\Vulkan\Lib" vulkan-1.lib user32.lib gdi32.lib kernel32.lib /OUT:Vk.exe 
+
+del Vk.obj Vk.pdb Vk.res
 
 Vk.exe
 
